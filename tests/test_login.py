7cb813bb182ccb52387b35cd4d7fe2cd461fef87
DmitriySabelnikov <Dmitriy.Sabelnikov@rt.ru>
2020-05-30 01:08:17 +0300

    update test_login - don't use ChromeDriverManager

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,11 +1,10 @@
 import pytest
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
 
 
 @pytest.fixture
 def driver(request):
-    wd = webdriver.Chrome(ChromeDriverManager().install())
+    wd = webdriver.Chrome()
     request.addfinalizer(wd.quit)
     return wd
 
