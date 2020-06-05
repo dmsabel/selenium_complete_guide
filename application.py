7ede75c87120e6e4997278dec5d107f910e09565
DmitriySabelnikov <Dmitriy.Sabelnikov@rt.ru>
2020-06-05 23:27:53 +0300

    updated app fixture to work with multiple browsers

--- application.py
+++ application.py
@@ -5,8 +5,14 @@ from helpers.auth import AuthHelper
 
 class Application:
 
-    def __init__(self):
-        self.wd = webdriver.Chrome()
+    def __init__(self, browser="chrome"):
+        if browser == "chrome":
+            self.wd = webdriver.Chrome()
+        elif browser == "firefox":
+            self.wd = webdriver.Firefox(firefox_binary="C:/Program Files/Mozilla Firefox/firefox.exe")
+        elif browser == "ie":
+            self.wd = webdriver.Ie()
+
         self.wd.implicitly_wait(2)
 
         self.auth = AuthHelper(self.wd)
--- conftest.py
+++ conftest.py
@@ -3,13 +3,15 @@ from selenium import webdriver
 
 from application import Application
 
-fixture = Application()
-
 
 @pytest.fixture()
 def app(request):
     def teardown():
         fixture.wd.quit()
+    try:
+        fixture = Application(request.param)
+    except AttributeError:
+        fixture = Application()
     request.addfinalizer(teardown)
     return fixture
 
